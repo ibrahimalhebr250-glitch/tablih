133d73020a4244386e5f5ee11a1da18d7f757941
ibrahimalhebr250-glitch <ibrahimalhebr250@gmail.com>
2026-03-07 04:21:05 +0300

    Updated RatingDialog.tsx

--- src/components/deals/shared/RatingDialog.tsx
+++ src/components/deals/shared/RatingDialog.tsx
@@ -83,7 +83,7 @@ export default function RatingDialog({
 
   return (
     <div
-      className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4"
+      className="fixed inset-0 bg-black/60 flex items-center justify-center z-[300] p-4"
       onClick={onClose}
     >
       <div
